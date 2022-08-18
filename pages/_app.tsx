a3efdba26e49181a5e1310c6347ed1e92fcae2a2
S.A.E.D <S.A.E.D@DESKTOP-7H26SAC>
2022-08-18 18:23:31 +0300

    add the Components and initial Nav and SideBar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import {useState, useEffect} from 'react';
+import NavBar from '../components/NavBar';
+import SideBar from '../components/SideBar';
 function MyApp({ Component, pageProps }: AppProps) {
   const [isSSR, setIsSSR] = useState(true);
   
@@ -9,7 +11,21 @@ function MyApp({ Component, pageProps }: AppProps) {
   }, []);
 
   if(isSSR) return null;
-  return <Component {...pageProps} />
+  return (
+  <div>
+    <NavBar/>
+    <div className='flex gap-6 md:gap-20'>
+      <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>
+        <SideBar/>
+        </div> 
+        <div className='mt-4 flex flex-col gap10 overflow0auto h-[88vh] video flex-1'>
+           <Component {...pageProps} />
+        </div>
+    </div>
+  </div>
+ )
+  
+  
 }
 
 export default MyApp
