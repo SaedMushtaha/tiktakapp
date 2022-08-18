a6fd6400e94c00db312da52b0f86844d754402fe
S.A.E.D <S.A.E.D@DESKTOP-7H26SAC>
2022-08-18 17:12:53 +0300

    initial project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-
+import {useState, useEffect} from 'react';
 function MyApp({ Component, pageProps }: AppProps) {
+  const [isSSR, setIsSSR] = useState(true);
+  
+  useEffect(() => {
+    setIsSSR(false);
+  }, []);
+
+  if(isSSR) return null;
   return <Component {...pageProps} />
 }
 
