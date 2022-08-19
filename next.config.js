54b1786b61900f415a5daa40fb3b368151549c74
S.A.E.D <S.A.E.D@DESKTOP-7H26SAC>
2022-08-19 14:32:36 +0300

    edit the nav bar and add zustand

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-    swcMinify: true,
-  images:{
-    domains: ['yt3.ggpht.com'],
-  }
+  swcMinify: true,
+  images: {
+    domains: ["yt3.ggpht.com", "lh3.googleusercontent.com", "www.seekpng.com"],
+  },
+};
 
-}
-
-module.exports = nextConfig
+module.exports = nextConfig;
